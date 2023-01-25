fa4cb3f4e7ddf5504db3470e6e02570af6c6e307
Put your Name in Here <your@email.here>
2023-01-25 22:27:00 +0300

    proje okey

--- src/Task.js
+++ src/Task.js
@@ -1,18 +1,44 @@
 import React from "react";
+import { formatDistanceToNow, differenceInDays } from "date-fns";
+import { tr } from "date-fns/locale";
 
 const Task = ({ taskObj, onComplete }) => {
+  let kalanZaman = formatDistanceToNow(new Date(taskObj.deadline), {
+    locale: tr,
+    addSuffix: true,
+  });
+
+  let aradaGun = differenceInDays(new Date(taskObj.deadline), new Date());
+
+  let stil;
+
+  if (aradaGun <= 3) {
+    stil = {
+      backgroundColor: "#ffd9d4",
+    };
+  } else {
+    stil = {
+      backgroundColor: "#d4d7ff",
+    };
+  }
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">
+        son teslim: <span style={stil}>{kalanZaman}</span>
+      </div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className="pill" key={p}>
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>
+      )}
     </div>
   );
 };
